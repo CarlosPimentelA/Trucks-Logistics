cce5e10a5cb151f18371de7f9feaa8b8766f3c98
carlospimentel.a <157527363+CarlosPimentelA@users.noreply.github.com>
2026-04-11 19:51:13 -0400

    Implementacion de variables de entorno con .env.

---
+++ src/main/java/com/trucks_logistics/Trucks/Logistics/config/EnvConfig.java
@@ -0,0 +1,18 @@
+package com.trucks_logistics.Trucks.Logistics.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+
+    private final Dotenv dotenv = Dotenv.load();
+
+    @PostConstruct
+    public void init() {
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    }
+}
