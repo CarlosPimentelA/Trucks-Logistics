c139acceaf54db250cb0884ef1aeb85dd55ff19d
carlospimentel.a <157527363+CarlosPimentelA@users.noreply.github.com>
2026-05-01 21:29:18 -0400

    Add spring security to the pom.xml

---
+++ src/main/java/com/trucks_logistics/Trucks/Logistics/users/User.java
@@ -0,0 +1,14 @@
+package com.trucks_logistics.Trucks.Logistics.users;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+}
---
+++ src/main/java/com/trucks_logistics/Trucks/Logistics/users/UserRepository.java
@@ -0,0 +1,7 @@
+package com.trucks_logistics.Trucks.Logistics.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
