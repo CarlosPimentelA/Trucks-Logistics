9ad55c4311bb3a82425dff44cb70c37ebef7b5bc
carlospimentel.a <157527363+CarlosPimentelA@users.noreply.github.com>
2026-05-04 21:08:16 -0400

    Terminamos la implementacion del email de verificacion de cuenta.
    Issues #6 y 7

--- src/main/java/com/trucks_logistics/Trucks/Logistics/email/EmailService.java
+++ src/main/java/com/trucks_logistics/Trucks/Logistics/email/EmailService.java
@@ -14,7 +14,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void enviarCodigoVerificacion(String emailDestino, int codigo) {
+    public void enviarCodigoVerificacion(String emailDestino, String verificationLink) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
@@ -27,16 +27,17 @@ public class EmailService {
             String htmlContent = """
                     <div style="font-family: Arial; background-color: #f4f4f4; padding: 20px;">
                         <div style="background-color: #ffffff; padding: 40px; border-radius: 10px; text-align: center; max-width: 500px; margin: auto;">
-                            <h2 style="color: #333;">Verifica tu cuenta de trazo</h2>
-                            <p>Gracias por registrarte. Tu código de verificación es:</p>
-                            <div style="font-size: 32px; font-weight: bold; background: #eee; padding: 10px; margin: 20px; border-radius: 5px;">
-                                %d
-                            </div>
-                            <p style="font-size: 13px; color: #666;">Este código expira pronto.</p>
+                            <h2 style="color: #333;">Verifica tu cuenta en Trazo 🚛</h2>
+                            <p>Gracias por registrarte. Haz click en el botón para verificar tu cuenta:</p>
+                            <a href="%s" style="display: inline-block; background-color: #333; color: white; padding: 12px 24px; border-radius: 5px; text-decoration: none; margin: 20px 0;">
+                                Verificar mi cuenta
+                            </a>
+                            <p style="font-size: 13px; color: #666;">Este link expira en 24 horas.</p>
+                            <p style="font-size: 13px; color: #666;">Si no creaste esta cuenta ignora este email.</p>
                         </div>
                     </div>
                     """
-                    .formatted(codigo);
+                    .formatted(verificationLink);
 
             helper.setText(htmlContent, true); // El 'true' es vital: indica que es HTML
 
